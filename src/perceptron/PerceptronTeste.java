f777c31aa07e7ea2f07bc214d497c2cc22a8c882
Rafael Tadeu <faeltadeu@hotmail.com>
2017-11-06 11:53:17 -0200

    Conclusão da implementação Processo ainda apresenta erros. A corrigir.

--- src/perceptron/PerceptronTeste.java
+++ src/perceptron/PerceptronTeste.java
@@ -16,40 +16,72 @@ import java.util.Scanner;
  * @author FaelT
  */
 public class PerceptronTeste {
-        static double entradas[][] = new double[20][20];
-        static int saidas[] = new int[20];
+        static double entradasArquivo[][] = new double[100][100];
+        static int saidasArquivo[] = new int[20];
+        static double [][] entradas;
+        static int [] saidas;
+        static int contador = 0;
+        static int variaveis = 0;
     public static void lerArquivo(String nome) throws FileNotFoundException, IOException{
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\FaelT\\Google Drive\\PUC\\2017 -2\\Inteligência Artificial\\Trabalho Perceptron"+nome));
-        int contador = 0;
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\e.rafael.medeiros\\Documentos\\NetBeansProjects\\Perceptron\\src\\perceptron/"+nome));
+        contador = 0;
         while(br.ready()){
             String linha = br.readLine();
+            
             contador ++;
             String [] valores = linha.split(" ");
-            for(int i = 0; i<valores.length -1; i++){
-                entradas[contador][i] = Double.parseDouble(valores[i]);
+            variaveis = valores.length-1;
+            for(int i = 0; i<=variaveis; i++){
+                entradasArquivo[contador][i] = Double.parseDouble(valores[i]);
             }
-            saidas[contador] = Integer.parseInt(valores[valores.length]);
+            saidasArquivo[contador] = Integer.parseInt(valores[valores.length]);
             System.out.println(linha);
         }
         br.close();
     }
     
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException{
         Perceptron p = new Perceptron();
         //double entradas[][]={{0,0},{0,1},{1,0},{1,1}};
         //int saidas[] = {1,1,1,0};
        // double entradas[][];
         //int saidas[];
+       
+        
+        
+        
+        
         Scanner in = new Scanner(System.in);
         String linha;
         System.out.println("Informe o nome do arquivo desejado");
         linha = in.next();
         
-        p.treinar(entradas, saidas, 0.2, 0.1, 200);
-        System.out.println(p.Saida(new double[]{0,0}));
-        System.out.println(p.Saida(new double[]{1,0}));
-        System.out.println(p.Saida(new double[]{0,1}));
-        System.out.println(p.Saida(new double[]{1,1}));
+        lerArquivo(linha);
+         for(int i = 0; i<variaveis; i++){
+             for(int j=0; j<variaveis; j++){
+                 entradas[i][j] = entradasArquivo[i][j];
+             }
+        }
+         for(int i=0; i<contador; i++){
+             saidas[i] = saidasArquivo[i];
+         }
+        
+         for(int i = 0; i<variaveis; i++){
+             for(int j=0; j<variaveis; j++){
+                 System.out.print(entradas[i][j]);
+                 
+             }
+             System.out.println("");
+        }
+         for(int i=0; i<contador; i++){
+             System.out.print(saidas[i]);
+         }
+         
+       // p.treinar(entradas, saidas, 0.2, 0.1, 200);
+        //System.out.println(p.Saida(new double[]{0,0}));
+        //System.out.println(p.Saida(new double[]{1,0}));
+        //System.out.println(p.Saida(new double[]{0,1}));
+        //System.out.println(p.Saida(new double[]{1,1}));
     }
     
 }
